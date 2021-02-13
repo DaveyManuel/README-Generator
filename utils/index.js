95caf5ff830bf8ad86422f0c3f77857b2fddc039
David Sanchez <davidmsanchez250@gmail.com>
2021-02-12 20:21:20 -0500

    added preliminary  questions to const questions

--- utils/index.js
+++ utils/index.js
@@ -1,10 +1,45 @@
+const inquirer = require('inquirer')
+const fs = require('fs')
+
 // array of questions for user
 const questions = [
+
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What is the name of your project?'
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Please write a brief description for your project.'
+    },
+    {
+        type: 'input',
+        name: 'motivation',
+        message: 'What was the motivation to create your project?'
+    },
+    {
+        type: 'input',
+        name: 'userStory',
+        message: 'Provide a brief User Story'
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'Provide steps and instructions for use.'
+    },
+    {
+        type: 'input',
+        name: 'credits',
+        message: 'Please provide the names of those who helped you with this project.'
+    }
 
 ];
 
 // function to write README file
 function writeToFile(fileName, data) {
+
 }
 
 // function to initialize program
